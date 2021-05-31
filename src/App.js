30e4e05fe6574ea2c400573d8572ccfb2cb8ed36
Anup Mishra <anup@localhost.localdomain>
2021-05-31 22:02:16 +0530

    react amazon clone project

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React,{useEffect} from "react";
+import "./App.css";
+import Header from "./Header";
+import Login from "./Login"
+import Checkout from "./Checkout";
+import {useStateValue} from "./StateProvider";
+import Home from "./Home";
+import {auth} from './firebase';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Payment from './Payment';
+function App() {
+  const [{},dispatch] = useStateValue();
+  useEffect(()=>{
+    auth.onAuthStateChanged(authUser =>{
+      console.log("the user is " , authUser);
+      if(authUser){
+           dispatch({
+             type: 'SET_USER',
+             user: authUser
+           })
+      }else{
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  },[])
+  return (
+    <Router>
+      <div className="App">
+
+        <Switch>
+        <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/checkout">
+          <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+          <Header />
+            <Payment />
+          </Route>
+          <Route path="/">
+          <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Payment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Payment.css";
+function Payment(){
+    return(
+        <div>
+
+        </div>
+    )
+} 
+export default Payment;
